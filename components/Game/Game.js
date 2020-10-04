0ff2d784eefdfb2edbb0d343df8415340e768e9a
Marcelo Rodriguez <cmarcelorp@gmail.com>
2020-10-04 18:24:00 -0300

    Initial game state

--- components/Game/Game.js
+++ components/Game/Game.js
@@ -6,6 +6,7 @@ import Selector from "../Selector/Selector";
 import styles from "./Game.module.css";
 
 const SIZE = 9;
+const INITIAL_NUMBERS_COUNT = 10;
 
 const isBrowser = typeof window !== "undefined";
 
@@ -23,7 +24,7 @@ const Game = () => {
   };
 
   const handleRestart = () => {
-    setValue(getEmptyMatrix());
+    setValue(generateRandomGame());
   };
 
   useEffect(() => {
@@ -71,8 +72,32 @@ const getInvalidValues = (values, [column, row]) => {
   return invalid;
 };
 
-const getEmptyMatrix = () =>
-  Array.from({ length: SIZE }, (_, i) => [...Array(SIZE)]);
+const getRandomInt = (min = 1, max = 9) => {
+  const minAbs = Math.ceil(min);
+  const maxAbs = Math.floor(max);
+  return Math.floor(Math.random() * (maxAbs - minAbs + 1)) + minAbs;
+};
+
+const generateRandomGame = () => {
+  const matrix = Array.from({ length: SIZE }, (_, i) => [...Array(SIZE)]);
+  for (let i = 0; i < INITIAL_NUMBERS_COUNT; i++) {
+    let position;
+    let isPositionInvalid = false;
+    do {
+      position = [getRandomInt(0, 8), getRandomInt(0, 8)];
+      isPositionInvalid =
+        typeof matrix[position[0]][position[1]] !== "undefined";
+    } while (isPositionInvalid);
+    let value;
+    let isValueInvalid;
+    do {
+      value = getRandomInt();
+      isValueInvalid = getInvalidValues(matrix, position).indexOf(value) !== -1;
+    } while (isValueInvalid);
+    matrix[position[0]][position[1]] = value;
+  }
+  return matrix;
+};
 
 const saveGameStatus = (gameStatus) => {
   isBrowser &&
@@ -81,5 +106,5 @@ const saveGameStatus = (gameStatus) => {
 
 const getGameStatus = () => {
   const gameStatus = isBrowser && window.localStorage.getItem("game-status");
-  return gameStatus ? JSON.parse(gameStatus) : getEmptyMatrix();
+  return gameStatus ? JSON.parse(gameStatus) : generateRandomGame();
 };
